def189f6051912f9044e370c489b8ee4c8e88f54
Diego Ferrari Bruno <75423232+diegoferraribruno@users.noreply.github.com>
2022-08-01 19:36:51 -0300

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,19 @@
+doElsCollide = function(el1, el2) {
+    el1.offsetBottom = el1.offsetTop + el1.offsetHeight;
+    el1.offsetRight = el1.offsetLeft + el1.offsetWidth;
+    el2.offsetBottom = el2.offsetTop + el2.offsetHeight;
+    el2.offsetRight = el2.offsetLeft + el2.offsetWidth;
+    if (el1.offsetBottom > el2.offsetBottom){
+        el1.style.zIndex = 1;
+        el2.style.zIndex = 0;
+    };
+    if (el1.offsetBottom < el2.offsetBottom){
+        el1.style.zIndex = 0;
+        el2.style.zIndex = 1;
+    };
+    return !((el1.offsetBottom < el2.offsetTop) ||
+             (el1.offsetTop > el2.offsetBottom) ||
+             (el1.offsetRight < el2.offsetLeft) ||
+             (el1.offsetLeft > el2.offsetRight))
+
+};
